3d181025d76e461a5dd933452fcb720b918b4ef5
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-06-15 20:41:43 -0700

    Fixed the button

--- app/page.js
+++ app/page.js
@@ -10,9 +10,7 @@ export default function Home() {
           <a href="#menu">Menu & Pastries</a>
           <a href="#contact">Contact</a>
           <a href="#about">About Us</a>
-          <div className = "button">
-            <button type="submit">Order Online</button>
-          </div>
+          <button type="submit" className="button">Order Online</button>
         </div>
         <img src="https://assets.cntraveller.in/photos/618e82491af22a472dbf4f9e/4:3/w_1792,h_1344,c_limit/Cafe%20Duco%20Interiors-10.jpg" alt="restaurant image" class="cafe"></img>
       </div>
