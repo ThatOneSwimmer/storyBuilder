46fd6fe18945ef172396429ea344954c14ceb71f
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-06-20 21:32:28 -0700

    added routes and cleaned up page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,14 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main className= "main">
         <div className="topnav">
-          <a href="#space">The Space</a>
-          <a href="#menu">Menu & Pastries</a>
-          <a href="#contact">Contact</a>
-          <a href="#about">About Us</a>
+          <a href="/thespace">The Space</a>
+          <a href="/menu">Menu & Pastries</a>
+          <a href="/contact">Contact</a>
+          <a href="/about">About Us</a>
           <button type="submit" className="button">Order Online</button>
         </div>
         <img src="https://assets.cntraveller.in/photos/618e82491af22a472dbf4f9e/4:3/w_1792,h_1344,c_limit/Cafe%20Duco%20Interiors-10.jpg" alt="restaurant image" class="cafe"></img>
-      </div>
     </main>
   )
 }
---
+++ pages/thespace.js
@@ -0,0 +1,5 @@
+export default function TheSpace() {
+    return (
+        <h1>Welcome to the Space</h1>
+    );
+  }
