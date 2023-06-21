0eb940bfb4bade0522a189ebf622e3c1fc125906
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-06-21 13:38:10 -0700

    added my webpage stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+export default function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className= "main">
+        <div className="topnav">
+          <a href="/thespace">The Space</a>
+          <a href="/menu">Menu & Pastries</a>
+          <a href="/contact">Contact</a>
+          <a href="/about">About Us</a>
+          <button type="submit" className="button">Order Online</button>
+        </div>
+        <img src="https://assets.cntraveller.in/photos/618e82491af22a472dbf4f9e/4:3/w_1792,h_1344,c_limit/Cafe%20Duco%20Interiors-10.jpg" alt="restaurant image" class="cafe"></img>
+    </main>
+  )
 }
-
-export default App;
