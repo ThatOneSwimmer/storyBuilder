cebf4194da4eddd7b9c644e62ce0f402544ab59e
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-06-24 14:55:13 -0700

    added pages files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,7 @@ export function Home() {
       <img src="https://assets.cntraveller.in/photos/618e82491af22a472dbf4f9e/4:3/w_1792,h_1344,c_limit/Cafe%20Duco%20Interiors-10.jpg" alt="restaurant image" className="cafe" />
       <h1 className = "image-text"> Ready to serve you.</h1>
       <button type="submit" className="second-button">Order Online</button>
-      <p className= "first-paragraph"> Located in the historic Glass Building, in the trendy
-       WareHouse District of downtown Charlottesville, VA. </p>
-       <img src="https://cdn.discordapp.com/attachments/1113663834310316112/1121285918305828964/newcrop.jpg" alt="restaurant image" className="java" />
+       <img src="https://b.zmtcdn.com/data/pictures/9/20571879/6bbacbecf776cade9100813838a7157d.jpg" alt="restaurant image" className="second-image" />
       <p className= "second-paragraph"> Offers a quiet space with comfy armchairs, a large farm 
       table for groups, a wall-side bar and café tables. Outdoor seating is available weather
        permitting and we certainly encourage taking one's espresso and croissants outside on a gorgeous day! </p>
