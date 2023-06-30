e6cefd97b68e5ddddf6927cb87e6eaa5b737755c
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-06-29 20:28:24 -0700

    wrapped main app with context

--- src/App.js
+++ src/App.js
@@ -4,27 +4,31 @@ import { Menu } from "./pages/Menu"
 import { Home } from "./pages/Home"
 import { Contact } from "./pages/Contact"
 import { About } from "./pages/About"
+import React, { useContext } from 'react';
+import { ReferenceDataContext, ReferenceDataContextProvider } from "./ReferenceDataContext";
 
 
-function App() {
+export default function App() {
+  const { userData } = useContext(ReferenceDataContext)
   return (
-    <main className= "main">
-        <div className="topnav">
-          <a href="/">Home</a>
-          <a href="/menu">Menu & Pastries</a>
-          <a href="/contact">Contact</a>
-          <a href="/about">About Us</a>
-          <button type="submit" className="first-button">Order Online</button>
-        </div>
-    <Routes>
-      <Route path = "" Route element={<Home />} />
-      <Route path ="/menu" element={<Menu/>} />
-      <Route path ="/contact" element={<Contact/>} />
-      <Route path ="/about" element={<About/>} />
-      <Route/>
-    </Routes>
-    </main>
+    <ReferenceDataContextProvider>
+      <main className= "main">
+          <div className="topnav">
+            <a href="/">Home</a>
+            <a href="/menu">Menu & Pastries</a>
+            <a href="/contact">Contact</a>
+            <a href="/about">About Us</a>
+            <button type="submit" className="first-button">Order Online</button>
+          </div>
+      <Routes>
+        <Route path = "" Route element={<Home />} />
+        <Route path ="/menu" element={<Menu/>} />
+        <Route path ="/contact" element={<Contact/>} />
+        <Route path ="/about" element={<About/>} />
+        <Route/>
+      </Routes>
+      </main>
+    </ReferenceDataContextProvider>
   )
 }
 
-export default App
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,25 @@
+import React, { useContext } from 'react';
+import { ReferenceDataContext } from '../ReferenceDataContext';
+
 export function Contact() {
-    return (
-        <div className="contact-page">
-        <img src="https://media.tenor.com/Os38-K7VHL4AAAAd/shibainu-typing.gif" alt="restaurant image" className="dog" />
-        </div>
-    );
-  }
+  const { userData } = useContext(ReferenceDataContext);
+
+  return (
+    <div className="contact-page">
+      <img
+        src="https://media.tenor.com/Os38-K7VHL4AAAAd/shibainu-typing.gif"
+        alt="restaurant image"
+        className="dog"
+      />
+      <p>Search Input: {userData && userData.myInput}</p>
+      {/* checks if userData is empty and grabs the input from the search bar*/}
+    </div>
+  );
+}
+
+
+
+
+
+
+
