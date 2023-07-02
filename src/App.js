d79178101fd34c66b7055d064743c600dfd0ba04
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-07-02 15:40:58 -0700

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css'
-import { Route, Routes } from "react-router-dom"
-import { Menu } from "./pages/Menu"
-import { Home } from "./pages/Home"
-import { Contact } from "./pages/Contact"
-import { About } from "./pages/About"
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './pages/Menu';
+import { Home } from './pages/Home';
+import { Contact } from './pages/Contact';
+import { About } from './pages/About';
 import React, { useContext } from 'react';
-import { ReferenceDataContext, ReferenceDataContextProvider } from "./ReferenceDataContext";
-
+import { ReferenceDataContext, ReferenceDataContextProvider } from './ReferenceDataContext';
 
 export default function App() {
-  const { userData } = useContext(ReferenceDataContext)
+  const { userData } = useContext(ReferenceDataContext);
+
   return (
     <ReferenceDataContextProvider>
-      <main className= "main">
+      <main className="main">
+        {/* Conditionally render the menu bar */}
+        {window.location.pathname !== '/about' && (
           <div className="topnav">
             <a href="/">Home</a>
             <a href="/menu">Menu & Pastries</a>
@@ -20,15 +22,15 @@ export default function App() {
             <a href="/about">About Us</a>
             <button type="submit" className="first-button">Order Online</button>
           </div>
-      <Routes>
-        <Route path = "" Route element={<Home />} />
-        <Route path ="/menu" element={<Menu/>} />
-        <Route path ="/contact" element={<Contact/>} />
-        <Route path ="/about" element={<About/>} />
-        <Route/>
-      </Routes>
+        )}
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route />
+        </Routes>
       </main>
     </ReferenceDataContextProvider>
-  )
+  );
 }
-
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,25 @@
 export function About() {
     return (
         <div className="about-page">
+            <div className="contact-navbar">
+                <a href="/">Home</a>
+                <a href="/achievements">Achievements</a>
+                <a href="/help">Help/FAQ</a>
+                <a href="/about">About Us</a>
+            </div>
+            <section className = "description-container" >
+                <h3>The Storybuilder</h3>
+                <span> an online playground </span>
+                <hr>
+                </hr>
+                <p>
+                    write a brief description of the app here lol 
+                    I want to go pet capybaras again :U 
+                    also I love ducks they're my second favorite animal and I draw them on whiteboards at plu :O
+                </p>
+                <button type="button" class = "b">Get started! </button>
+            </section>
+            <img src="../images/1.jpg" alt="playground img"></img>
         </div>
     );
   }
