0c4f2267b302359c077841be21763ed6bcf7f1cb
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-07-14 15:04:41 -0700

    added images, fonts, and homepage html

--- chatbot/src/Pages/Main.js
+++ chatbot/src/Pages/Main.js
@@ -1,33 +1,31 @@
 import { useNavigate } from 'react-router-dom';
+import image1 from '../images/1.jpg';
 
 const Main = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: '10vh',
-        }}
-      >
-        <h1>Story Builder</h1>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: '20vh',
-        }}
-      >
-        <button onClick={() => navigate('/readinglvl')}>
-          Start
-        </button>
-      </div>
-    </>
+    <div className="about-page">
+    <section className="about-navbar">
+        <a href="/">Home</a>
+        <a href="/achievements">Achievements</a>
+        <a href="/help">Help/FAQ</a>
+        <a href="/about">About Us</a>
+    </section>
+    <section className = "description-container" >
+        <h3>The Storybuilder</h3>
+        <span> an online playground </span>
+        <hr>
+        </hr>
+        <p>
+            write a brief description of the app here lol 
+            I want to go pet capybaras again :U 
+            also I love ducks they're my second favorite animal and I draw them on whiteboards at plu :O
+        </p>
+        <a href="/display" class="b">Get started!</a>
+    </section>
+    <img src={image1} alt="playground img" className = "playground-img" />
+</div>
   );
 };
 
