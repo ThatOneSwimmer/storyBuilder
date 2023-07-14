36bed89fa4ace60b3e150de794917288f290497d
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-07-14 14:07:03 -0700

    dino webpage now up

--- src/pages/Display.js
+++ src/pages/Display.js
@@ -2,37 +2,36 @@ import image1 from '../images/abstract.jpg';
 
 export function Display() {
     return (
-    <div className="display-page">
-        <section className= "left-side">
-        <div className= "chat"> 
-            <h3> Chat </h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p>
-        </div>
-        <div className= "cam">
-            <div className = "info">
-                <h3> cam </h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        <main class="main">
+        <div class="main">
+          <div class="display-page">
+            <section class="left-side">
+              <div class="chat">
+                <h3>Chat</h3>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                  industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                  scrambled it to make a type specimen book.
                 </p>
-            </div>
-        </div>
-    </section>
-    <section className= "right-side">
-    <div className= "play">
-            <div className = "info">
-                <h3> cam </h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </div>
+            </section>
+            <section class="play">
+              <div class="info">
+                <h3>Now Playing...</h3>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                  industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                  scrambled it to make a type specimen book.
                 </p>
-            </div>
+                <div class="display-buttons">
+                  <button type="button" class="display-b">Home</button>
+                  <button type="button" class="display-b">Redo</button>
+                  <button type="button" class="display-b">Start Newtesting</button>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>
-    </section>
-    </div>
-       
+      </main>
     );
 }
