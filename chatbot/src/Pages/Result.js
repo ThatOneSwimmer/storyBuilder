f3ec446c4c3d5f569767ee3aa131688bba5784ef
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-07-15 14:31:01 -0700

    Updated the  chatgpt moldel - send mult calls now

--- chatbot/src/Pages/Result.js
+++ chatbot/src/Pages/Result.js
@@ -20,15 +20,23 @@ const Result = () => {
     const [notShowing, setNotShowing] = useState(true);
 
     const response = async() => {
-        try {
-            const result = await openai.createCompletion({
-              model: "text-davinci-003",
-              prompt: "Write a " + readinglvl + " grade reading level story about a " + animal + 
-                      " named " + name + " using a childish writing style have the prompt be about 4 short sentences.",
-              temperature: 0.5,
-              max_tokens: 4000,
+         try {
+            const result = await openai.createChatCompletion({
+              model: 'gpt-3.5-turbo',
+              messages: [{role: "system", content: "you are an author writing a story for a specific grade level."},
+              {role: "user", content: "Write a " + readinglvl + " grade reading level story about a " + animal + 
+                       " named " + name + " using a childish writing style have the prompt be about 4 short sentences."}
+            ],
+              // model: "gpt-3.5-turbo",
+              // model: "text-davinci-003",
+              // prompt: "Write a " + readinglvl + " grade reading level story about a " + animal + 
+              //         " named " + name + " using a childish writing style have the prompt be about 4 short sentences.",
+              // messages: [{role: "user", content: "you are an author writing a story for a specific grade level."},],
+              // {"role": "user", "content": "Write a grade reading level story about a named using a childish writing style have the prompt be about 4 short sentences."},],
+              // temperature: 0.5,
+              // max_tokens: 4000,
             });
-            setApiResponse(result.data.choices[0].text);
+            setApiResponse(result.data.choices[0].message.content);
           } catch (e) {
             setApiResponse("Something is going wrong, Please try again.");
           }
