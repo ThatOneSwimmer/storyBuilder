fd9495fff86ba5d3ef8ccf2bb132d36837698d0a
nguyenmx <125946317+nguyenmx@users.noreply.github.com>
2023-07-28 16:07:09 -0700

    added some styling to the animal page

--- chatbot/src/Pages/Animal.js
+++ chatbot/src/Pages/Animal.js
@@ -1,14 +1,32 @@
 import { useNavigate } from 'react-router-dom';
 import { useContext } from "react";
 import { ReferenceDataContext } from "../ReferenceDataContext";
+import seal from '../images/seal-no-outline.png';
+import unicorn from '../images/unicorn.png';
+import bear from '../images/polar-bear.png';
+import dog from '../images/walking-the-dog.png';
+import cat from '../images/cat.png';
 
 const Animal = () => {
     const { animal, setAnimal } = useContext(ReferenceDataContext);
     const navigate = useNavigate();
 
     return (
-        <>
-            <div
+        <div className = "animal-page">
+            <h3>
+                CHOOSE YOUR ANIMAL
+            </h3>
+            <section className = "animals-container"> 
+                <img src={seal} alt="seal img" className = "seal-img" />
+                <img src={unicorn} alt="unicorn img" className = "unicorn-img" />
+                <img src={bear} alt="bear img" className = "bear-img" />
+                <img src={dog} alt="dog img" className = "dog-img" />
+                <img src={cat} alt="cat img" className = "cat-img" />
+            </section>
+            
+            
+            {/* Lilyan's code*/}
+            {/* <div
                 style={{
                     display: "flex",
                     justifyContent: "center",
@@ -35,8 +53,8 @@ const Animal = () => {
                 <button onClick={() => navigate('/name')}>
                     Next
                 </button>
-            </div>
-        </>
+            </div> */}
+    </div>
     )
 }
 
